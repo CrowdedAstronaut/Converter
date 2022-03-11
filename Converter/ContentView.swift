3bb69e89a9d2081471088fbfefecf172565549b7
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-11 07:58:15 -0600

    Starting with data

--- Converter/ContentView.swift
+++ Converter/ContentView.swift
@@ -8,14 +8,47 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	@State private var input = 100.0
+	@State private var inputUnit = "Meters"
+	@State private var outputUnit = "Kilometers"
+	
+	let units = ["Feet", "Kilometers", "Meters", "Miles", "Yards"]
+	
+	var body: some View {
+		NavigationView {
+			Form {
+				Section {
+					TextField("Amount", value: $input, format: .number)
+						.keyboardType(.decimalPad)
+				} header: {
+					Text("Amount to convert")
+				}
+				
+				Picker("Convert from", selection: $inputUnit) {
+					ForEach(units, id: \.self) {
+						Text($0)
+					}
+				}
+				
+				Picker("Convert to", selection: $outputUnit) {
+					ForEach(units, id: \.self) {
+						Text($0)
+					}
+				}
+				
+				Section {
+					Text("???")
+				} header: {
+					Text("Result")
+				}
+			}
+			.navigationTitle("Converter")
+		}
+	}
+	
+	struct ContentView_Previews: PreviewProvider {
+		static var previews: some View {
+			ContentView()
+		}
+	}
 }
