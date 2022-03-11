d3538f8751611da59dd99e85a6cdb9fb90a15d5a
CrowdedAstronaut <ross@johnrossphillips.com>
2022-03-11 07:44:10 -0600

    Initial Commit

---
+++ Converter/ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConverterApp.swift
+//  Converter
+//
+//  Created by Ross Phillips on 3/11/22.
+//
+
+import SwiftUI
+
+@main
+struct ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
